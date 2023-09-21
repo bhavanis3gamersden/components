1353ef4fceef4e630042d513109f6f59a6da64e1
bhavanis3gamersden <142201755+bhavanis3gamersden@users.noreply.github.com>
2023-09-21 05:25:46 -0700

    Add your commit

---
+++ jsx/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+
+function MyComponent () {
+  return(
+    <div className='movie-card'>
+      <img src='https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg'/>
+      <h1 className='head'>Movie</h1>
+      <label className='para'>122.k</label>
+    </div>
+
+    
+  );
+}
+
+
+  
+
+export default MyComponent
